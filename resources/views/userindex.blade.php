13e8b3e8569a457d797e2fd2f22c62e760d60129
Hozuki Kosugi <hozuki.22.kousugi@gmail.com>
2024-01-10 20:03:09 +0900

    ffff

--- resources/views/userindex.blade.php
+++ resources/views/userindex.blade.php
@@ -16,22 +16,19 @@
             <ul>    
                 <li>{{ $user->user_id }}</li>
                 @foreach($followusers as $followuser)
-                    @if($user->user_id !== $followuser->followed_id && $loginuser->user_id !== $followuser->followed_id)    
-                    @continue
-                        
-                    @elseif( $followuser->follower_id == $loginuser->user_id && $user->user_id == $followuser->followed_id )
-                        <form action="{{ route('follow.delete',$followuser) }}" method="POST">
-                            @csrf
-                            <button type="submit">フォロー解除</button>
-                            
-                        </form>
-                    @elseif( $followuser->follower_id == $loginuser->user_id && $user->user_id != $followuser->followed_id || )
-                    <form action="{{ route('user.follow') }}" method="POST">
+                    @if($followusers !== $loginuser->user_id && $followusers !== $user->user_id)
+                        <form action="{{ route('user.follow') }}" method="POST">
                             @csrf
                                 <input type="hidden" name="user_id" value="{{ $user->user_id }}">
                                 <button type="submit">フォローする</button>
                         </form>
-                    
+                    @elseif( $followuser->follower_id == $loginuser->user_id && $user->user_id == $followuser->followed_id )
+                        <form action="{{ route('follow.delete',$followuser) }}" method="POST">
+                            @csrf
+                            <button type="submit">フォロー解除</button>
+                        </form>
+                    @elseif($user->user_id !== $followuser->followed_id )
+                        @continue
                     @endif  
                 @endforeach 
             </ul>
