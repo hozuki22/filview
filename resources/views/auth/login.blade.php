beea65ccf0e3a524b1382265e86483481783438f
Hozuki Kosugi <hozuki.22.kousugi@gmail.com>
2024-02-18 18:37:23 +0900

    ログイン画面　必要情報変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,37 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <x-guest-layout>
+    <x-slot name="header">
+        <div id="header_inner">
+            <h1 id="headline"><a href="">filview</a></h1>
+        </div>
+    </x-slot>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
+    <h1>ログイン</h1>
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
+        <!-- UserID -->
+        <div>
+            <x-input-label for="email" :value="__('User_name')" />
+            <x-text-input id="user_name" class="form-control" type="text" name="user_name" :value="old('user_name')" required autofocus />
+            <x-input-error :messages="$errors->get('user_name')" class="mt-2" />
+        </div>
+        
+
+        {{-- <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+        </div> --}}
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="form-control"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
@@ -39,10 +53,15 @@
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-
-            <x-primary-button class="ms-3">
+            <br>
+            <x-primary-button class="btn btn-primary btn-lg">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+        <x-slot name="footer">
+            <div id="footer">
+                <p>&copy;2024 hozuki</p>
+            </div>
+        </x-slot>
 </x-guest-layout>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
