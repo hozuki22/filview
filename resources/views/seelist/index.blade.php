3ebd13293ad4e292a7f930797c31b3600a7b428c
Hozuki Kosugi <hozuki.22.kousugi@gmail.com>
2024-02-19 23:14:45 +0900

    見たいリスト画面実装

--- resources/views/seelist/index.blade.php
+++ resources/views/seelist/index.blade.php
@@ -1,12 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>これはテストです。</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.app')
+@section('title','見たいリスト')
+@section('content')
+    <h1>見たいリスト</h1>
+    <div>
+        @foreach($cinemas as $cinema)
+            <div>
+                <ul>
+                    <li><img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/{{ $cinema['poster_path'] }}" alt="ポスターパス"></li>
+                    <li>{{ $cinema['title'] }}</li>
+                    <br>
+                    <a href="{{ route('review.create',$cinema['id']) }} "><button type="submit">見た</button></a>
+                    <a href="{{ route('wanttosee.store',$cinema['id']) }}"><button type="submit">見たい</button></a>
+                </ul>
+            </div>
+        @endforeach
+    </div>
+
+@endsection
